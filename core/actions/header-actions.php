f6a6dee224af59bb9f3c2bab3058022f6e8e7e1f
Tyler Cunningham <tyler@cyberchimps.com>
2012-03-28 11:50:02 -0700

    google font ssl fix

--- core/actions/header-actions.php
+++ core/actions/header-actions.php
@@ -212,7 +212,7 @@ function business_link_rel() {
 
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-<link href='http://fonts.googleapis.com/css?family=<?php echo $font ; ?>' rel='stylesheet' type='text/css' /> <?php
+<link href='//fonts.googleapis.com/css?family=<?php echo $font ; ?>' rel='stylesheet' type='text/css' /> <?php
 }
 
 
