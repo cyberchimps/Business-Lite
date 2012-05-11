ce23f66ec48e8ef065c94c025dbf3567dd335e05
Tyler Cunningham <tyler@cyberchimps.com>
2012-05-11 13:18:42 -0700

    responsive video option, polish language file

--- core/core-functions.php
+++ core/core-functions.php
@@ -61,6 +61,12 @@ function business_scripts() {
 	wp_enqueue_script ('modernizr');
 	wp_enqueue_script ('menu');
 	wp_enqueue_script ('mobilemenu');
+	
+	if ($options->get($themeslug.'_responsive_video') == '1' ) {
+	
+		wp_register_script( 'video' ,$path.'/js/video.js');
+		wp_enqueue_script ('video');	
+	}
 
 	if ($options->get($themeslug.'_lazy_load') == '1' ) {
 		wp_register_script( 'lazyload' ,$path.'/js/jquery.lazyload.js');
--- includes/classy-options-init.php
+++ includes/classy-options-init.php
@@ -50,6 +50,7 @@ $options
 	->section("Design")
 		->open_outersection()
 			->select($themeslug."_color_scheme", "Select a Skin Color", array( 'options' => array("light" => "Light (default)", "dark" => "Dark"), 'default' => 'light'))
+			->checkbox($themeslug."_responsive_video", "Responsive Videos")
 		->close_outersection()
 		->subsection("Typography")
 			->select($themeslug."_font", "Choose a Font", array( 'options' => array("Arial" => "Arial (default)", "Courier New" => "Courier New", "Georgia" => "Georgia", "Helvetica" => "Helvetica", "Lucida Grande" => "Lucida Grande", "Tahoma" => "Tahoma", "Times New Roman" => "Times New Roman", "Verdana" => "Verdana", "Maven+Pro" => "Maven Pro", "Ubuntu" => "Ubuntu")))
