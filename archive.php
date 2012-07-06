777f89e3b0d2838669e30a34d7107c1dfab46e80
mfssurya <suryap@mindfiresolutions.com>
2012-07-06 20:01:45 +0530

    - Added "push" div for sticky footer.

--- archive.php
+++ archive.php
@@ -79,4 +79,8 @@
 		<?php if ($options->get($themeslug.'_archive_breadcrumbs') == "1") { business_breadcrumbs();}?>
 </div><!--end container-->
 
+<!-- For sticky footer -->
+<div class="push"></div>  
+</div>	<!-- End of wrapper -->
+
 <?php get_footer(); ?>
