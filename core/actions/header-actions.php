c2952ab0d1d964324cdcbdc59529d4883eaeccd7
hugosaner <hugo@sanerdesign.com>
2012-10-15 16:05:41 +0100

    made changes that w3 validation brought up
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/actions/header-actions.php
+++ core/actions/header-actions.php
@@ -60,12 +60,11 @@ function business_font() {
 */
 function business_html_attributes() { ?>
 <!doctype html>
-<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html class=""> <!--<![endif]-->
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes('xhtml'); ?>>
+<!--[if lt IE 7 ]> <html class="ie6" <?php language_attributes('xhtml'); ?>> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7" <?php language_attributes('xhtml'); ?>> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8" <?php language_attributes('xhtml'); ?>> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9" <?php language_attributes('xhtml'); ?>> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]> <html xmlns="http://www.w3.org/1999/xhtml"<?php language_attributes('xhtml'); ?>> <![endif]-->
 <head profile="http://gmpg.org/xfn/11"> <?php 
 }
 
