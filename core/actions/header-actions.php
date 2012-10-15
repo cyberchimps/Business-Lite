1533ed32f5a9721c44c0e62e32f42e7c26f0c4c9
hugosaner <hugo@sanerdesign.com>
2012-10-15 16:19:00 +0100

    header - removed changes so that they can be added to official release next week
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/actions/header-actions.php
+++ core/actions/header-actions.php
@@ -60,14 +60,12 @@ function business_font() {
 */
 function business_html_attributes() { ?>
 <!doctype html>
-<!--[if lt IE 7 ]> <html class="ie6" <?php language_attributes('xhtml'); ?>> <![endif]-->
-<!--[if IE 7 ]>    <html class="ie7" <?php language_attributes('xhtml'); ?>> <![endif]-->
-<!--[if IE 8 ]>    <html class="ie8" <?php language_attributes('xhtml'); ?>> <![endif]-->
-<!--[if IE 9 ]>    <html class="ie9" <?php language_attributes('xhtml'); ?>> <![endif]-->
-<!--[if gt IE 9]>  <html <?php language_attributes(); ?>> <![endif]-->
-<!--[if !IE]><!--> 
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html class=""> <!--<![endif]-->
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes('xhtml'); ?>>
 <head profile="http://gmpg.org/xfn/11"> <?php 
 }
 
