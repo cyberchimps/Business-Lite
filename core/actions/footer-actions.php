2be91d0983564420dfc3d11a5a4863c3f34f3334
hugosaner <hugo@sanerdesign.com>
2012-11-08 16:46:49 +0000

    footer actions - corrected code errors
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/actions/footer-actions.php
+++ core/actions/footer-actions.php
@@ -33,31 +33,31 @@ function business_footer_widgets() {
    	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer") ) { ?>
 		
 		<div class="three columns footer-widgets">
-			<h3 class="footer-widget-title"><?php printf( __( 'Footer Widgets', 'business' )); ?></h3>
+			<h3 class="footer-widget-title"><?php _e( 'Footer Widgets', 'business' ); ?></h3>
 			<ul>
 				<li>To customize this widget area login to your admin account, go to Appearance, then Widgets and drag new widgets into Footer Widgets</li>
 			</ul>
 		</div>
 
 		<div class="three columns footer-widgets">
-			<h3 class="footer-widget-title"><?php printf( __( 'Recent Posts', 'business' )); ?></h3>
+			<h3 class="footer-widget-title"><?php _e( 'Recent Posts', 'business' ); ?></h3>
 			<ul>
 				<?php wp_get_archives('type=postbypost&limit=4'); ?>
 			</ul>
 		</div>
 
 		<div class="three columns footer-widgets">
-			<h3 class="footer-widget-title"><?php printf( __( 'WordPress', 'business' )); ?></h3>
+			<h3 class="footer-widget-title"><?php _e( 'WordPress', 'business' ); ?></h3>
 			<ul>
     		<?php wp_register(); ?>
     		<li><?php wp_loginout(); ?></li>
-    		<li><a href="<?php echo esc_url( __('http://wordpress.org/', 'business' )); ?>" target="_blank" title="<?php esc_attr_e('Powered by WordPress, state-of-the-art semantic personal publishing platform.', 'business'); ?>"> <?php printf( __('WordPress', 'business' )); ?></a></li>
+    		<li><a href="<?php echo esc_url( __('http://wordpress.org/', 'business' )); ?>" target="_blank" title="<?php esc_attr_e('Powered by WordPress, state-of-the-art semantic personal publishing platform.', 'business'); ?>"> <?php _e('WordPress', 'business' ); ?></a></li>
     		<?php wp_meta(); ?>
     		</ul>
 		</div>
 		
 		<div class="three columns footer-widgets">
-			<h3 class="footer-widget-title"><?php printf( __( 'Search', 'business' )); ?></h3>
+			<h3 class="footer-widget-title"><?php _e( 'Search', 'business' ); ?></h3>
 			<?php get_search_form(); ?>
 		</div>
 		
