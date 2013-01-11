2b13ff70335316f0a886d76aae85637c26b8e2f5
hugosaner <hugo@sanerdesign.com>
2013-01-11 15:29:54 +0000

    Sidebar - swapped printf() for _e()
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- sidebar.php
+++ sidebar.php
@@ -3,28 +3,28 @@
         <!-- All this stuff in here only shows up if you DON'T have any widgets active in this zone -->
     
 		<div class="widget-container">    
-		<h2 class="widget-title"><?php printf( __('Pages', 'business' )); ?></h2>
+		<h2 class="widget-title"><?php _e('Pages', 'business' ); ?></h2>
 		<ul>
     	<?php wp_list_pages('title_li=' ); ?>
     	</ul>
     	</div>
     
 		<div class="widget-container">    
-    	<h2 class="widget-title"><?php printf( __( 'Archives', 'business' )); ?></h2>
+    	<h2 class="widget-title"><?php _e( 'Archives', 'business' ); ?></h2>
     	<ul>
     		<?php wp_get_archives('type=monthly'); ?>
     	</ul>
     	</div>
         
 		<div class="widget-container">    
-       <h2 class="widget-title"><?php printf( __('Categories', 'business' )); ?></h2>
+       <h2 class="widget-title"><?php _e('Categories', 'business' ); ?></h2>
         <ul>
     	   <?php wp_list_categories('show_count=1&title_li='); ?>
         </ul>
         </div>
         
 		<div class="widget-container">    
-    	<h2 class="widget-title"><?php printf( __('WordPress', 'business' )); ?></h2>
+    	<h2 class="widget-title"><?php _e('WordPress', 'business' ); ?></h2>
     	<ul>
     		<?php wp_register(); ?>
     		<li><?php wp_loginout(); ?></li>
@@ -34,10 +34,10 @@
     	</div>
     	
     	<div class="widget-container">
-    	<h2 class="widget-title"><?php printf( __('Subscribe', 'business' )); ?></h2>
+    	<h2 class="widget-title"><?php _e('Subscribe', 'business' ); ?></h2>
     	<ul>
-    		<li><a href="<?php bloginfo('rss2_url'); ?>"><?php printf( __('Entries (RSS)', 'business' )); ?></a></li>
-    		<li><a href="<?php bloginfo('comments_rss2_url'); ?>"><?php printf( __('Comments (RSS)', 'business' )); ?></a></li>
+    		<li><a href="<?php bloginfo('rss2_url'); ?>"><?php _e('Entries (RSS)', 'business' ); ?></a></li>
+    		<li><a href="<?php bloginfo('comments_rss2_url'); ?>"><?php _e('Comments (RSS)', 'business' ); ?></a></li>
     	</ul>
     	</div>
 	
