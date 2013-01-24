2735ecd5643934211bc3e17b09c3c4e35c407909
hugosaner <hugo@sanerdesign.com>
2013-01-24 17:18:09 +0000

    reduce calls to get_template_directory()
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- functions.php
+++ functions.php
@@ -52,18 +52,18 @@ add_action('after_setup_theme', 'business_text_domain');
 */ 
 function business_theme_setup() {
 	
-
+	$directory = get_template_directory();
 // Initialize business Core Framework and Pro Extension.
  
-	require_once ( get_template_directory() . '/core/core-init.php' );
+	require_once ( $directory . '/core/core-init.php' );
 
 
 // Call additional files required by theme.
  
-	require_once ( get_template_directory() . '/includes/classy-options-init.php' ); // Theme options markup.
-	require_once ( get_template_directory() . '/includes/options-functions.php' ); // Custom functions based on theme options.
-	require_once ( get_template_directory() . '/includes/meta-box.php' ); // Meta options markup.
-	require_once ( get_template_directory() . '/includes/presstrends.php' ); // Opt-in PressTrends option.
+	require_once ( $directory . '/includes/classy-options-init.php' ); // Theme options markup.
+	require_once ( $directory . '/includes/options-functions.php' ); // Custom functions based on theme options.
+	require_once ( $directory . '/includes/meta-box.php' ); // Meta options markup.
+	require_once ( $directory . '/includes/presstrends.php' ); // Opt-in PressTrends option.
 
 	add_theme_support(
 		'post-formats',
