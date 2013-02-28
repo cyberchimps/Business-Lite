5bc5e372bf4ceb76f87ab0e85b5752b27efe08a7
marutim <marutim@mindfiresolutions.com>
2013-02-28 17:19:20 +0530

    Replaced get_template_directory_uri() with '$root', the variable defined with the same value.

--- functions.php
+++ functions.php
@@ -129,7 +129,7 @@ function business_custom_gallery_post_format( $content ) {
 				<div class="entry nine columns">
 					<?php if ($options->get($themeslug.'_post_formats') == '1') : ?>
 						<div class="postformats"><!--begin format icon-->
-							<img src="<?php echo get_template_directory_uri(); ?>/images/formats/gallery.png" />
+							<img src="<?php echo $root; ?>/images/formats/gallery.png" />
 						</div><!--end format-icon-->
 					<?php endif;?>
 					<h2 class="posts_title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
