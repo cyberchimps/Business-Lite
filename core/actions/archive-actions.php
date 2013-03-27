e309973751d4871eb6b49a9f9788309f06751c83
marutim <marutim@mindfiresolutions.com>
2013-03-27 15:45:24 +0530

    Removed unused global variable '$post'

--- core/actions/archive-actions.php
+++ core/actions/archive-actions.php
@@ -26,28 +26,28 @@ add_action( 'business_archive_title', 'business_archive_page_title' );
 * @since 3.0
 */
 function business_archive_page_title() { 
-	global $post; ?>
+	?>
 	
-		<?php if (is_category()) { ?>
+		<?php if( is_category() ) { ?>
 			<h2 class="archivetitle"><?php _e( 'Archive for the &#8216;', 'business' ); ?><?php single_cat_title(); ?><?php _e( '&#8217; Category:', 'business' ); ?></h2><br />
 
 		<?php } elseif( is_tag() ) { ?>
 			<h2 class="archivetitle"><?php _e( 'Posts Tagged &#8216;', 'business' ); ?><?php single_tag_title(); ?><?php _e( '&#8217;:', 'business' ); ?></h2><br />
 
-		<?php } elseif (is_day()) { ?>
-			<h2 class="archivetitle"><?php _e( 'Archive for', 'business' ); ?> <?php the_time('F jS, Y'); ?>:</h2><br />
+		<?php } elseif( is_day() ) { ?>
+			<h2 class="archivetitle"><?php _e( 'Archive for', 'business' ); ?> <?php the_time( 'F jS, Y' ); ?>:</h2><br />
 
-		<?php } elseif (is_month()) { ?>
-			<h2 class="archivetitle"><?php _e( 'Archive for', 'business' ); ?> <?php the_time('F, Y'); ?>:</h2><br />
+		<?php } elseif( is_month() ) { ?>
+			<h2 class="archivetitle"><?php _e( 'Archive for', 'business' ); ?> <?php the_time( 'F, Y' ); ?>:</h2><br />
 
-		<?php } elseif (is_year()) { ?>
-			<h2 class="archivetitle"><?php _e( 'Archive for:', 'business' ); ?> <?php the_time('Y'); ?>:</h2><br />
+		<?php } elseif( is_year() ) { ?>
+			<h2 class="archivetitle"><?php _e( 'Archive for:', 'business' ); ?> <?php the_time( 'Y' ); ?>:</h2><br />
 
-		<?php } elseif (is_author()) { ?>
+		<?php } elseif( is_author() ) { ?>
 			<h2 class="archivetitle"><?php _e( 'Author Archive:', 'business' ); ?></h2><br />
 
-		<?php } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-			<h2 class="archivetitle"><?php _e('Blog Archives:', 'business' ); ?></h2><br />
+		<?php } elseif( isset( $_GET['paged'] ) && !empty( $_GET['paged'] ) ) { ?>
+			<h2 class="archivetitle"><?php _e( 'Blog Archives:', 'business' ); ?></h2><br />
 	
 		<?php } 
 }
